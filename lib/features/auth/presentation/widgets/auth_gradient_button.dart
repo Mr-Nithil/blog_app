fe0ecee472642d322bd796ca6bce6f2b3a1a752b
Mr-Nithil <nithilsheshan@gmail.com>
2026-03-31 23:32:29 +0530

    Implement signup page and related UI components with theming

---
+++ lib/features/auth/presentation/widgets/auth_gradient_button.dart
@@ -0,0 +1,32 @@
+import 'package:blog_app/config/theme/app_palette.dart';
+import 'package:flutter/material.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  const AuthGradientButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [AppPalette.gradient1, AppPalette.gradient2],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(395, 55),
+          backgroundColor: AppPalette.transparentColor,
+          shadowColor: AppPalette.transparentColor,
+        ),
+        child: Text(
+          "Sign Up",
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
