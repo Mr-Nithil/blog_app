97927b3f9754808a47393ae0b10cea115e267448
Mr-Nithil <nithilsheshan@gmail.com>
2026-04-10 11:13:17 +0530

    feat: Implement blog listing with BlocConsumer for state management and loading indicators

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,6 +1,10 @@
+import 'package:blog_app/core/utils/show_snackbar.dart';
+import 'package:blog_app/core/widgets/loader.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_blog_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlogPage extends StatefulWidget {
   static rout() => MaterialPageRoute(builder: (context) => BlogPage());
@@ -11,6 +15,12 @@ class BlogPage extends StatefulWidget {
 }
 
 class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(BlogGetAll());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +35,28 @@ class _BlogPageState extends State<BlogPage> {
           ),
         ],
       ),
-      body: Center(child: Text("Blog Page")),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackBar(context, state.message);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+          if (state is BlogDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return Text(blog.title);
+              },
+            );
+          }
+          return const SizedBox();
+        },
+      ),
     );
   }
 }
